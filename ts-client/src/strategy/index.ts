35a53d9323dc6d96ebd411253467445cccb4b1fc
Andrew Nguyen <cuong.hv.dtvt@gmail.com>
2022-05-31 17:28:07 +0800

    Add more functions ts client

---
+++ ts-client/src/strategy/index.ts
@@ -0,0 +1,74 @@
+import { BN } from "@project-serum/anchor";
+import { AnchorDefined } from "@saberhq/anchor-contrib";
+import { PublicKey, TransactionInstruction } from "@solana/web3.js";
+import { STRATEGY_PROGRAM_ADDRESSES } from '../constants';
+import type { VaultProgram } from "../vault";
+import MangoHandler from "./mango";
+import { IDL, Vault as VaultIdl } from "../idl";
+import PortWithoutLMHandler from "./portWithoutLM";
+import SolendWithoutLMHandler from "./solendWithoutLM";
+import VaultHandler from "./vault";
+
+export type StrategyType =
+  | "portFinanceWithoutLm"
+  | "solendWithoutLm"
+  | "mango"
+  | "vault";
+
+export type StrategyState = {
+  reserve: PublicKey;
+  collateralVault: PublicKey;
+  strategyType: object;
+  bumps: Uint8Array;
+  currentLiquidity: BN;
+};
+
+export type ReserveState = {
+  collateral: {
+    mintPubkey: PublicKey;
+    mintTotalSupply: number;
+    supplyPubkey: String;
+  };
+  state: unknown;
+};
+export interface StrategyHandler {
+  strategyProgram?: PublicKey;
+  withdraw(
+    program: VaultProgram,
+    strategy: any,
+    vault: PublicKey,
+    tokenVault: PublicKey,
+    feeVault: PublicKey,
+    lpMint: PublicKey,
+    userToken: PublicKey,
+    userLp: PublicKey,
+    amount: number,
+    preInstructions: TransactionInstruction[],
+    postInstructions: TransactionInstruction[]
+  ): Promise<string>;
+}
+
+export const getStrategyType = (strategyResponse: any) => {
+  return Object.keys(strategyResponse)[0] as StrategyType;
+};
+
+export const getStrategyHandler = (
+  strategyType: StrategyType,
+  strategyProgramAddresses: {
+    solend: PublicKey;
+    portFinance: PublicKey;
+  } = STRATEGY_PROGRAM_ADDRESSES
+): VaultHandler | null => { 
+  switch (strategyType) {
+    case "solendWithoutLm":
+      return new SolendWithoutLMHandler(strategyProgramAddresses.solend);
+    case "portFinanceWithoutLm":
+      return new PortWithoutLMHandler(strategyProgramAddresses.portFinance);
+    case "mango":
+      return new MangoHandler();
+    case "vault":
+      return new VaultHandler();
+    default:
+      return null;
+  }
+};
--- ts-client/tests/vault.test.ts
+++ ts-client/tests/vault.test.ts
@@ -1,35 +1,84 @@
-import * as anchor from "@project-serum/anchor";
-import { Connection, Keypair } from "@solana/web3.js";
-import { expect } from "chai";
+import { Connection, Keypair, PublicKey } from "@solana/web3.js";
+import SolanaTokenList from "@solana/spl-token-registry/dist/module/tokens/solana.tokenlist.json";
+import { Wallet, Provider } from "@project-serum/anchor";
+
 import Vault from "../src/vault";
 import { SOL_MINT } from "../src/constants";
+import { airDropSol } from './utils';
 
-const mockWallet = new anchor.Wallet(Keypair.generate());
-const mockConnection = new Connection("https://api.mainnet-beta.solana.com");
+const mockWallet = new Wallet(new Keypair());
+const mainnetConnection = new Connection("https://api.mainnet-beta.solana.com");
+const devnetConnection = new Connection("https://api.devnet.solana.com/");
+const SOL_TOKEN_INFO = SolanaTokenList.tokens.find(token => token.symbol === 'SOL');
 
-test("Vault State", async () => {
-  const vault = new Vault(mockWallet, mockConnection);
-  await vault.getVaultStateByMint(SOL_MINT);
+beforeAll(async () => {
+  await airDropSol(devnetConnection, mockWallet.publicKey);
+})
 
-  const lpSupply = (await mockConnection.getTokenSupply(vault.state.lpMint))
-    .value.amount;
-  console.log("total lp supply: ", lpSupply);
-
+describe('Get Mainnet vault state', () => {
+  const provider = new Provider(mainnetConnection, mockWallet, {
+    commitment: "processed",
+  });
+  const vault = new Vault(provider);
+  
   const currentTime = Math.floor(Date.now() / 1000);
-  const unlockedAmount = vault.getUnlockedAmount(currentTime);
-  console.log("total unlocked amount: ", unlockedAmount);
+  let lpSupply;
+  beforeAll(async () => {
+    await vault.getVaultStateByMint(SOL_MINT);
+    lpSupply = (await mainnetConnection.getTokenSupply(vault.state.lpMint)).value.amount;
+  })
 
-  const amountByShare = vault.getAmountByShare(
-    currentTime,
-    100_000_000,
-    lpSupply
-  );
-  console.log("amount by share: ", amountByShare);
+  test("lp supply", async () => {
+    expect(typeof lpSupply).toBe("string");
+  });
 
-  const unmintAmount = vault.getUnmintAmount(
-    currentTime,
-    100_000_000,
-    lpSupply
-  );
-  console.log("unmint amount: ", unmintAmount);
-});
+  test("get unlocked amount", async () => {
+    const unlockedAmount = vault.getUnlockedAmount(currentTime);
+    expect(typeof unlockedAmount).toBe("number");
+  })
+
+  test("get amount by share", async () => {
+    const amountByShare = vault.getAmountByShare(
+      currentTime,
+      100_000_000,
+      lpSupply
+    );
+    expect(typeof amountByShare).toBe("number");
+  })
+
+  test("get unmint amount", async () => {
+    const unMintAmount = vault.getUnmintAmount(
+      currentTime,
+      100_000_000,
+      lpSupply
+    );
+    expect(typeof unMintAmount).toBe("number");
+  })
+})
+
+describe('Interact with Vault', () => {
+  const provider = new Provider(devnetConnection, mockWallet, {
+    commitment: "confirmed",
+  });
+  const vault = new Vault(provider);
+
+  test("Vault Withdraw SOL", async () => {
+    const depositResult = await vault.deposit(SOL_TOKEN_INFO, 2000);
+    expect(typeof depositResult).toBe("string");
+    const withdrawResult = await vault.withdraw(SOL_TOKEN_INFO, 1000);
+    expect(typeof withdrawResult).toBe("string");
+  });
+
+  test("Vault Withdraw SOL from strategy", async () => {
+    await vault.getVaultStateByMint(SOL_MINT);
+    for (var strategy of vault.state.strategies) {
+      if (!strategy.equals(PublicKey.default)) {
+        console.log("Test with ", strategy.toString());
+        const depositResult = await vault.deposit(SOL_TOKEN_INFO, 2000);
+        expect(typeof depositResult).toBe("string");
+        const withdrawResult = await vault.withdrawFromStrategy(SOL_TOKEN_INFO,strategy, 1000);
+        expect(typeof withdrawResult).toBe("string");
+      }
+    }
+  });
+})
