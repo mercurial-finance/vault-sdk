6c4194c1fe3d94ddb71e1466aed62900e0f05e61
Andrew Nguyen <cuong.hv.dtvt@gmail.com>
2023-01-21 14:35:16 +0700

    add frakt

---
+++ programs/vault/src/strategy/frakt.rs
@@ -0,0 +1,11 @@
+use crate::ID;
+use anchor_lang::prelude::Pubkey;
+/// return vault owner
+pub fn get_strategy_owner(strategy_pubkey: &Pubkey) -> (Pubkey, u8) {
+    Pubkey::find_program_address(&["frakt".as_ref(), strategy_pubkey.as_ref()], &ID)
+}
+
+/// liquidity pool bump
+pub const LIQUIDITY_POOL_BUMP: u8 = 0;
+/// price based pool bump
+pub const PRICE_BASED_POOL_BUMP: u8 = 1;
