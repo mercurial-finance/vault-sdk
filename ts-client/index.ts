b21f13664e9dd6ec1811fee9cdce0a28dcbd4693
McSam <mcsam1216@gmail.com>
2023-11-16 14:14:50 +0800

    feat: vault error class

--- ts-client/index.ts
+++ ts-client/index.ts
@@ -3,6 +3,7 @@ import { PROGRAM_ID, AFFILIATE_PROGRAM_ID, KEEPER_URL } from './src/vault/consta
 import { getVaultPdas, getOnchainTime, getLpSupply } from './src/vault/utils';
 import { getAmountByShare, getUnmintAmount, calculateWithdrawableAmount } from './src/vault/helper';
 import { IDL } from './src/vault/idl';
+import VaultError from './src/vault/error';
 
 export default VaultImpl;
 export {
@@ -19,6 +20,8 @@ export {
   getAmountByShare,
   getUnmintAmount,
   calculateWithdrawableAmount,
+  // Error
+  VaultError,
 };
 
 export type { VaultImplementation, VaultState, AffiliateInfo, ParsedClockState } from './src/vault/types';
